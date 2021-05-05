3b168a9a1ea579b585db4aa347d80cb98bbc4526
PauloCampos <paulo4-0@outlook.com>
2021-05-05 17:11:54 -0300

    chore: Initial my repositorie

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+import usePersistedState from './utils/usePersistedState';
+
+import GlobalStyle from './styles/global'
+import Header from './components/header';
+
+const App = () => {
+  const [theme, setTheme] = usePersistedState('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />        
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
