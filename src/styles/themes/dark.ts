f0dccfb543663d363b3a6662fbb881ab8018ffed
PauloCampos <paulo4-0@outlook.com>
2021-05-05 17:15:33 -0300

    chore: include all my styles and dark and light themes

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    title: 'dark',
+  
+    colors: {
+      primary: '#6E44FF',
+      secundary: '#0B3948',
+  
+      background: '#483D3F',
+      text: '#fff',
+    },
+  };
